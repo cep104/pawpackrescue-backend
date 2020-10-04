6b00892aa3647e8626d9e5c9428c326e0eb14e4c
cep104 <candicelizabethpeters@gmail.com>
2020-10-04 16:38:36 -0500

    api/v1 and rack cors

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,8 @@
 class Dog < ApplicationRecord
     belongs_to :caretaker
     validates :name,:rescue_date,:gender,:breed,:size,:bio, presence: true
-
+    validates_inclusion_of :size, :in => ['small','medium','large']
+    validates_inclusion_of :gender, :in => ['male','female']
+    
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :users
-  resources :caretakers
+  namespace :api do
+    namespace :v1 do
+      resources :dogs
+      resources :users
+      resources :caretakers
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
