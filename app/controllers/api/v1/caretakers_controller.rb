2160de65576d9afbdb0b97c7c7d4b1d8f8a17969
cep104 <candicelizabethpeters@gmail.com>
2020-10-05 14:04:59 -0500

    bug fixes for backend

--- app/controllers/api/v1/caretakers_controller.rb
+++ app/controllers/api/v1/caretakers_controller.rb
@@ -1,44 +1,37 @@
 class Api::V1::CaretakersController < ApplicationController
     before_action :set_caretaker, only: [:show, :update, :destroy]
-    
     def index
-        @caretakers = Caretaker.all
-        render json: @caretakers
+        @caretakers = Caretaker.all 
+        render json:@caretakers
     end
 
     def create
         @caretaker = Caretaker.new(caretaker_params)
     
             if @caretaker.save
-                render json: @caretaker, status: :created, location: @caretaker
+                render json: @caretaker, status: :created, location: @dog
             else
                 render json: {error:'Error creating Foster'}
             end
     end
 
     def show
-        render json: @list
+        render json: @caretaker
     end
 
     def update
-        if @caretaker.update(caretaker_params)
-            render json: @caretaker
-          else
-            render json: @caretaker.errors, status: :unprocessable_entity
-          end
-
     end
 
     def destroy
-        @caretaker.destroy
     end
 
     private 
+
     def set_caretaker
         @caretaker = Caretaker.find(params[:id])
     end
 
-    def caretaker_params
-        params.require(:caretaker).permit(:name, :location, :setting)
+    def user_params
+        params.require(:user).permit(:name, :email, :password, dog_ids: [], dog_attributes:[:name, :bio])
     end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,10 +21,12 @@ class Api::V1::UsersController < ApplicationController
 
     private 
 
-    
+    def set_user
+        @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:name, :email, :password)
     end
-end
+
 end
