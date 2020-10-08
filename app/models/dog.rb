6fba069a029f120605c884515919fe3fbd0f7090
cep104 <candicelizabethpeters@gmail.com>
2020-10-08 18:15:13 -0500

    made user optional for dogs

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,6 @@
 class Dog < ApplicationRecord
     belongs_to :caretaker
+    belongs_to :user, optional: true
     validates :name,:rescue_date,:gender,:breed,:size,:bio, presence: true
     validates_inclusion_of :size, :in => ['small','medium','large']
     validates_inclusion_of :gender, :in => ['male','female']
