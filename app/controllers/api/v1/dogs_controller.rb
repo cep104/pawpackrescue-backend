f1cad063063ea6fc898fcb144019f73e6331b097
cep104 <candicelizabethpeters@gmail.com>
2020-10-13 17:33:22 -0500

    issue fixed with loaading

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -5,52 +5,40 @@ class Api::V1::DogsController < ApplicationController
         @dogs = Dog.all
         render json: @dogs
     end
-
     def create
-
-        @dog = @caretaker.dogs.new(dog_params)
-    
+        @dog = Dog.new(dog_params)
+        @dog.caretaker = Caretaker.find(params[:caretaker_id])
             if @dog.save
-                render json: @caretaker
+                render json: @dog
             else
                 render json: {error:'Error creating Dog'}
             end
     end
-
     def show
         dog = Dog.find(params[:id])
         render json: dog
     end
-
     def update
         if @dog.update(dog_params)
             render json: @dog
           else
             render json: @dog.errors, status: :unprocessable_entity
           end
-          
-
     end
-
     def destroy
         dog = Dog.find(params[:id])
         caretaker = Caretaker.find(dog.caretaker_id)
         dog.destroy
         render json: caretaker
     end
-
     private 
     def set_caretaker
         @caretaker = Caretaker.find(params[:caretaker_id])
     end
-
     # def set_dog
     #     @dog = Dog.find(params[:id])
     # end
-
     def dog_params
         params.require(:dog).permit(:name, :age, :gender, :breed, :house_trained, :good_with, :size, :bio, :medication, :caretaker_id, :img_src)
     end
-
-
 end
